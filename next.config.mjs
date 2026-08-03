722f25227e0c56ba8ff87b2628f165f6928110af
paradisesui <ju.cbr2@gmail.com>
2026-08-03 07:12:50 +0700

    fix globals css and postcss build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // ข้ามการตรวจ Type Checking เพื่อให้ผ่านการ Build บน Vercel
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ข้ามการตรวจ Linting เพื่อให้ผ่านการ Build บน Vercel
     ignoreDuringBuilds: true,
   },
 };
